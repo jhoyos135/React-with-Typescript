bc2726ccacf40184003124c3578bfdeb554c0439
Julian Hoyos <jhoyos135@gmail.com>
2019-07-20 16:05:09 -0400

    loading when fetching

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,14 +5,29 @@ import { StoreState } from '../reducers';
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
   deleteTodo: typeof deleteTodo;
 }
 
 class App extends Component<AppProps> {
+  state = {
+    fetching: false
+  };
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({
+      fetching: true
+    });
   };
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({
+        fetching: false
+      });
+    }
+  }
 
   onTodoClick = (todoId: number): void => {
     this.props.deleteTodo(todoId);
@@ -32,6 +47,7 @@ class App extends Component<AppProps> {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? 'LOADING...' : null}
         {this.renderList()}
       </div>
     );
