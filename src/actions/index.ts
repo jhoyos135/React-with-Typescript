80e54dc9310ab677969c07da724475df9a8e6b52
Julian Hoyos <jhoyos135@gmail.com>
2019-07-20 14:20:36 -0400

    action types with interfaces and enums

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,13 +1,26 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.FETCH_TODOS;
+  payload: Todo[];
+}
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(url);
-    dispatch({
-      type: 'FETCH_TODOS',
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.FETCH_TODOS,
       payload: response.data
     });
   };
